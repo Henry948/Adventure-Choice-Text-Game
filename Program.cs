d525a010ced8b5b6feb54d32d362bb77269470ee
dahdi <62443126+dahdi@users.noreply.github.com>
2021-01-14 20:09:14 +0000

    Create Program.cs and create base class

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ChooseYourOwnAdventure
+{
+  class Program
+  {
+      static void Main(string[] args)
+    {
+      // THE MYSTERIOUS NOISE 
+      Console.Write("What is your name?: ");
+      string name = Console.ReadLine();
+      Console.WriteLine($"Hello, {name}! Welcome to our story.");
+
+     
+    }
+  }
+}
+
